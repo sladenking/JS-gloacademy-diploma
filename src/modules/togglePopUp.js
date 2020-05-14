818d0f45a3279c55fe35d1692c752017d52bf3f3
Viacheslav Denysko <deniskosv@gmail.com>
2020-05-14 14:52:10 +0300

    done popup 4

--- src/modules/togglePopUp.js
+++ src/modules/togglePopUp.js
@@ -1,6 +1,7 @@
 const togglePopUp = () => {
   const popupCall = document.querySelector('.popup-call'),
     popupDiscount = document.querySelector('.popup-discount'),
+    popupCheck = document.querySelector('.popup-check'),
     popupContent = document.querySelector('.popup-content');
 
   document.body.addEventListener('click', event => {
@@ -19,6 +20,14 @@ const togglePopUp = () => {
     } else if (target.classList.contains('popup-close') || target.classList.contains('popup-discount')) {
       popupDiscount.style.display = 'none';
     } 
+
+    if (target.classList.contains('check-btn')) {
+      popupCheck.style.display = 'block';
+      popupContent.style.top = '10%';
+    } else if (target.classList.contains('popup-close') || target.classList.contains('popup-check')) {
+      popupCheck.style.display = 'none';
+    }
+
   });
 };
 
